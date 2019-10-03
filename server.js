85bc794c3bcef08947dd75051755b9ef70e12481
beck <kennedr6@gmatc.matc.edu>
2019-10-02 20:24:05 -0500

    added data validation to user registration endpoint

--- server.js
+++ server.js
@@ -38,8 +38,14 @@ app.post('/api/users',[
     check('email', 'Please enter a valid email').isEmail(),
     check('password', 'Please enter a password with 6 or more characters').isLength({ min: 6 })
 ], (req, res) => {
-    console.log(req.body);
-    res.send(req.body);
+    //console.log(req.body);
+    const errors = validationResult(req);
+    if (!errors.isEmpty()){
+        return res.status(422).json({ errors: errors.array() });
+    } else {
+        return res.send(req.body);
+    }
+    //res.send(req.body);
 });
 
 //connection listener
