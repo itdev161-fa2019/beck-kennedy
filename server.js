89d7b9501ac3815cb63cc722dcca96f3e0498e91
beck <kennedr6@gmatc.matc.edu>
2019-10-02 20:23:45 -0500

    added data validation to user registration endpoint

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import connectDatabase from './config/db.js';
+import { check, validationResult } from 'express-validator';
+
 
 //const express = require('express');
 //const db = require('config/db.js');
@@ -31,7 +33,11 @@ app.get('/', (req, res) =>
  * @route POST api/users
  * @desc register users
  */
-app.post('/api/users', (req, res) => {
+app.post('/api/users',[
+    check('name', 'Please enter your name').not().isEmpty(),
+    check('email', 'Please enter a valid email').isEmail(),
+    check('password', 'Please enter a password with 6 or more characters').isLength({ min: 6 })
+], (req, res) => {
     console.log(req.body);
     res.send(req.body);
 });
